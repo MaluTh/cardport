9fd7ab1be63a0ef98a6a92dff05e5ac0fe147976
Maria Luiza Theisges <malu.theisges@gmail.com>
2017-04-05 15:35:43 -0300

    Modificando a classe FIFO.

--- FIFO.h
+++ FIFO.h
@@ -13,35 +13,51 @@ template < int Q_SIZE > // O tamanho da final é passado como parâmetro
 
 class FIFO {
 public:
+	static const unsigned int FIFO_ERROR_EMPTY = -1;
+	static const unsigned int FIFO_ERROR_FULL= -2;
+
 	FIFO(){
 		_head = 0;
 		_tail = 0;
+		_length = 0;
+
 	}
 
 	~FIFO(){}
 
-	void push(char value){
-		if(((_tail+1)% Q_SIZE) == _head) return;
+	int push(char value){
 
-		_buffer[_tail] = value;
-
-		if(_tail == Q_SIZE) _tail = 0;
-		else _tail++;
-
+		if(_length == Q_SIZE) {
+			_tail = 0;
+			return FIFO_ERROR_FULL;
+		}else{
+			 _tail ++;
+			 int pos = _tail-1;
+			_buffer[pos] = value;
+			_length ++;
+			return pos;
+		}
 	}
 	char pop(){
 		int returnval;
-		if(_head == _tail) return 0;
+		if(_length == 0) return FIFO_ERROR_EMPTY;
 		returnval = _buffer[_head];
-		if(_head == Q_SIZE) _head =0;
-		else _head++;
 
+		if(_head == Q_SIZE) {
+			_head =0;
+		} else {
+			_head++;
+		}
+		_length --;
 		return returnval;
 
 	}
 
+	int size(){return _length;}
+	int max_size(){return Q_SIZE;}
+
 private:
-	int _head,_tail;
+	int _head,_tail, _length;
 	char _buffer[Q_SIZE];
 };
 
--- main.cpp
+++ main.cpp
@@ -2,72 +2,214 @@
  * main.cpp
  *
  *  Created on: 15 de mar de 2017
- *      Author: aluno
+ *      Author: arliones.hoeller
  */
 
-
-#include <util/delay.h>
-#include <avr/io.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
+
 #include "UART.h"
 #include "GPIO.h"
 #include "Timer.h"
 #include "FIFO.h"
-#include <avr/interrupt.h>
 
-UART uart(
-	19200,
-	UART::DATABITS_8,
-	UART::PARITY_NONE,
-	UART::STOPBITS_1
-);
-
-GPIO led (11,GPIO::OUTPUT);
-GPIO button (12, GPIO::INPUT);
+UART uart(19200, UART::DATABITS_8, UART::PARITY_NONE, UART::STOPBITS_1);
+GPIO led(11, GPIO::OUTPUT);
+GPIO botao(12, GPIO::INPUT);
 Timer timer(1000);
-FIFO<8> fila;
-
+bool val_botao;
 char message[8];
-char a;
 
-void setup(){
+void setup() {
 	sei();
-
 }
 
-bool val_botao;
+static const int MAX = 4;
+typedef FIFO<MAX> MyFIFO;
+MyFIFO fifo_test;
 
-
-
-void loop(){
-//	val_botao= button.get();
+void loop() {
+//	val_botao = botao.get();
 //	led.set(val_botao);
 //	timer.delay(1000);
-//	led.set(!val_botao);
-//	timer.delay(1000);
-//	sprintf(message,"%lu\n",timer.millis());
+//	sprintf(message, "%lu\n", timer.millis());
 //	uart.puts(message);
 
-	fila.push('1');
-	fila.push('2');
-	fila.push('3');
-	a = fila.pop();
-	printf("%c\n",a);
-	uart.put(a);
+	char x = 0, y = 0;
 
-	//uart.put('a\n');
+	uart.puts("Test 1: insert/remove 1.\r\n");
+//	fifo_test.clear();
 
-}
+	uart.puts("\t Push A. ");
+	fifo_test.push('A');
+	uart.puts(" => OK.\r\n");
 
-int main(){
-	setup();
-	while(true){
-		loop();
+	uart.puts("\t Pop: ");
+	x = fifo_test.pop();
+	uart.put(x);
+	if(x == 'A') uart.puts(". => OK.\r\n");
+	else uart.puts(". => NOK.\r\n");
+
+
+	uart.puts("Test 2: insert/remove MAX.\r\n");
+//	fifo_test.clear();
+
+	x = 'A';
+	for (int i = 0; i < MAX; i++) {
+		uart.puts("\t Push ");
+		uart.put(x);
+		uart.puts(". ");
+		fifo_test.push(x);
+		uart.puts(" => OK.\r\n");
+		x++;
 	}
 
+	y = 'A';
+	for (int i = 0; i < MAX; i++) {
+		uart.puts("\t Pop: ");
+		x = fifo_test.pop();
+		uart.put(x);
+		if(x == y) uart.puts(". => OK.\r\n");
+		else uart.puts(". => NOK.\r\n");
+		y++;
+	}
+
+
+	uart.puts("Test 3: remove from empty.\r\n");
+//	fifo_test.clear();
+
+	for (int i = 0; i < MAX; i++) {
+		uart.puts("\t Pop: ");
+		x = fifo_test.pop();
+		if(x == (char)MyFIFO::FIFO_ERROR_EMPTY) uart.puts(". => OK.\r\n");
+		else uart.puts(". => NOK.\r\n");
+	}
+
+	uart.puts("Test 4: insert in full.\r\n");
+	//fifo_test.clear();
+
+	x = 'A';
+	for (int i = 0; i < MAX; i++) {
+		uart.puts("\t Push ");
+		uart.put(x);
+		uart.puts(". ");
+		fifo_test.push(x);
+		uart.puts(" => OK.\r\n");
+		x++;
+	}
+
+	uart.puts("\t Add extra item: ");
+	x = fifo_test.push('X');
+	if(x == (char)MyFIFO::FIFO_ERROR_FULL) uart.puts(". => OK.\r\n");
+	else uart.puts(". => NOK.\r\n");
+
+	y = 'A';
+	for (int i = 0; i < MAX; i++) {
+		uart.puts("\t Pop: ");
+		x = fifo_test.pop();
+		uart.put(x);
+		if(x == y) uart.puts(". => OK.\r\n");
+		else uart.puts(". => NOK.\r\n");
+		y++;
+	}
+
+	timer.delay(10000);
+}
+
+int main() {
+	setup();
+	while (true)
+		loop();
 }
 
 
+
+
+///*
+// * main.cpp
+// *
+// *  Created on: 15 de mar de 2017
+// *      Author: aluno
+// */
+//
+//
+//#include <util/delay.h>
+//#include <avr/io.h>
+//#include <stdio.h>
+//#include "UART.h"
+//#include "GPIO.h"
+//#include "Timer.h"
+//#include "FIFO.h"
+//#include <avr/interrupt.h>
+//
+//UART uart(
+//	19200,
+//	UART::DATABITS_8,
+//	UART::PARITY_NONE,
+//	UART::STOPBITS_1
+//);
+//
+//GPIO led (11,GPIO::OUTPUT);
+//GPIO button (12, GPIO::INPUT);
+//Timer timer(1000);
+//FIFO<8> fila;
+//
+//char message[8];
+//char a;
+//
+//void setup(){
+//	sei();
+//
+//}
+//
+//bool val_botao;
+//
+//
+//
+//void loop(){
+////	val_botao= button.get();
+////	led.set(val_botao);
+////	timer.delay(1000);
+////	led.set(!val_botao);
+////	timer.delay(1000);
+////	sprintf(message,"%lu\n",timer.millis());
+////	uart.puts(message);
+////
+//
+////	fila.push('1');
+////	fila.push('2');
+////	fila.push('3');
+////	a = fila.pop();
+////	printf("%c\n",a);
+////	uart.put(a);
+//
+//	//uart.put('a\n');
+//
+//}
+//
+//int main(){
+//	setup();
+//	int i = 0;
+//
+//	while(i != fila.max_size()){
+//		fila.push(i);
+//		i++;
+//	}
+//
+//	while(fila.size() != 0){
+//		a = fila.pop();
+//		printf("%c\n",a);
+//		uart.put(a);
+//	}
+//
+////	while(true){
+////		loop();
+////	}
+//
+//
+//}
+//
+//
 
 
 
