a1ba9f2311977ca75341d8fc6a6625265f011a42
Maria Luiza Theisges <malu.theisges@gmail.com>
2017-05-19 10:59:52 -0300

    Ver porque o ADMIN não funciona e porque abre duas vezes.

--- main.cpp
+++ main.cpp
@@ -23,9 +23,9 @@ RDM6300<UART1> leitor(&rfid);
 
 GPIO led_vermelho(11, GPIO::OUTPUT);
 GPIO led_verde(9, GPIO::OUTPUT);
-GPIO botao1(12, GPIO::INPUT); //Adicionar
-GPIO botao2(13, GPIO::INPUT); //Abrir porta
-GPIO botao3(7, GPIO::INPUT); //Remove
+GPIO botao1(12, GPIO::INPUT); //Adicionar usuário
+GPIO botao2(13, GPIO::INPUT); //Abrir porta por dentro
+GPIO botao3(7, GPIO::INPUT); //Remover usuário
 GPIO rele(10, GPIO::OUTPUT);
 GPIO buzzer(8, GPIO::OUTPUT);
 
@@ -33,109 +33,124 @@ Timer timer(1000);
 bool val_botao;
 char message[8];
 char a;
-//unsigned long long adm = '00279176774866';
-
+unsigned long long adm = 3910811; // Administrador: cartão final 51
+//139179;
 
 Cadastro c;
+
+void aciona_buzzer(int time) {
+
+	int i = 0;
+	while (i != time) {
+		buzzer.set(1);
+		buzzer.set(0);
+		i++;
+	}
+
+}
+
+void acesso_negado() {
+
+	led_vermelho.set(1);
+	timer.delay(1000);
+	aciona_buzzer(5000);
+	timer.delay(500);
+	aciona_buzzer(5000);
+	led_vermelho.set(0);
+	leitor.set_current_id();
+
+}
+
+void acesso_permitido() {
+
+	led_verde.set(1);
+	timer.delay(1000);
+	aciona_buzzer(10000);
+	led_verde.set(0);
+	leitor.set_current_id();
+}
 
 void setup() {
 	sei();
 	// habilitando configurações globais SÓ FAZ ISSO AQUI
-
 	rele.set(1);
 	botao1.set(0);
 	botao2.set(0);
 	botao3.set(0);
-	//c.adiciona(adm);
+	c.adiciona(adm);
 }
 
 void loop() {
 
 	if (botao1.get() == true) {
 		while (botao1.get() == true) {
-			if(leitor.read()!= 0){
-				//if(leitor.get_current_id()==adm){
-					//uart.puts("ADM: Passe novo ID\n");
-					if(c.procura(leitor.get_current_id())==false){
+			if (leitor.read() != 0) {
+				if (leitor.get_current_id() == adm) {
+					uart.puts("ADM: Passe novo ID\n");
+					acesso_permitido();
+					if (c.procura(leitor.get_current_id()) == false) {
 						c.adiciona(leitor.get_current_id());
-						led_verde.set(1);
-						timer.delay(1000);
-						led_verde.set(0);
+
+						acesso_permitido();
 						uart.puts("ADM: Adicionado!\n");
 					}
-				/*}else{
+				} else {
 					uart.puts("Administrador NOK\n");
-				}*/
+					acesso_negado();
+				}
 			}
 		}
-		leitor.set_current_id();
 	}
 
-
 	if (botao2.get() == true) {
+
 		while (botao2.get() == true) {
 			rele.set(0);
 			timer.delay(500);
 			rele.set(1);
 		}
+
 	}
 
 	if (botao3.get() == true) {
 
 		while (botao3.get() == true) {
-			if (leitor.read()!=0) {
-				//if(leitor.get_current_id()== adm){
-					if(c.exclui(leitor.get_current_id())==true /*&& leitor.get_current_id()!=adm*/){
+			if (leitor.read() != 0) {
+				if (leitor.get_current_id() == adm) {
+					if (c.exclui(leitor.get_current_id()) == true
+							&& leitor.get_current_id() != adm) {
+
+						acesso_permitido();
 						uart.puts("ADM: Excluido\n");
-						leitor.set_current_id();
+
+					} else {
+
+						acesso_negado();
+						uart.puts("Não permitido excluir\n");
+
 					}
-				/*} else {
-					uart.puts("Não permitido excluir\n");
-				}*/
+				}
 			}
 		}
 	}
-
-
 
 	if (leitor.read() != 0) {
 
 		if (c.procura(leitor.get_current_id()) == true) {
 
-			led_verde.set(1); // acende led verde
-			timer.delay(1000);
-			led_verde.set(0); //desliga led
-			rele.set(0); // aciona relé
+			acesso_permitido();
+			rele.set(0);
 			timer.delay(500);
-			rele.set(1); // desliga relé
+			rele.set(1);
 			uart.puts("OK!\n");
-			int i = 0;
-			while(i != 10000){
-			 buzzer.set(1);
-			 buzzer.set(0);
-			 i++;
-			}
-
-
-			//timer.delay(1000);
 
 		} else {
 
-			led_vermelho.set(1);
-			buzzer.set(1);
-			timer.udelay(1000);
-			led_vermelho.set(0);
-			buzzer.set(0);
+			acesso_negado();
 			uart.puts("NOK!\n");
 
 		}
-		leitor.set_current_id();
-
 	}
-
-
-
-
 }
 
 int main() {
